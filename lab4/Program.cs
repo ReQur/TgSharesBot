a5834962df4facd8b75bb11c549589517216f996
Danil <xdd_280@mail.ru>
2021-04-17 17:47:27 +0500

    Code was cleaned up

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -3,46 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.Serialization.Formatters;
 using HtmlAgilityPack;
 using Telegram.Bot;
-using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InputFiles;
-using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Args;
 
 namespace lab4
 {
-    class Program
+    internal class Program
     {
-        const string HelpDesk = "\t/share *name of share* - returns extended information about the share\n" +
-                                "\t/add *name of share* [*name of share*...] - adds one or several shares in list\n" +
-                                "\t/list shows you all short information about all shares in list\n" +
-                                "\t/delete *name of share* [*name of share*...] - deletes one or several shares from list\n" +
-                                "\t/delete &all - clean list of shares";
-        class Share
-        {
-            private string _name;
-            public string Name
-            {
-                get { return _name; }
-                set { _name = value; }
-            }
-
-            private int _cost;
-            public int Cost
-            {
-                get { return _cost; }
-                set { _cost = value; }
-            }
-        }
+        private const string HelpDesk = "\t/share *name of share* - returns extended information about the share\n" +
+                                        "\t/add *name of share* [*name of share*...] - adds one or several shares in list\n" +
+                                        "\t/list shows you all short information about all shares in list\n" +
+                                        "\t/delete *name of share* [*name of share*...] - deletes one or several shares from list\n" +
+                                        "\t/delete &all - clean list of shares";
 
         private static List<Share> AddedShares = new List<Share>();
-        private static int SharesQuant = - 1;
+        private static int SharesQuant = -1;
 
-        static void Main(string[] args)
+        private static void Main()
         {
-
-            var web = new HtmlWeb()
+            var web = new HtmlWeb
             {
                 PreRequest = OnPreRequest
             };
@@ -65,22 +45,22 @@ namespace lab4
         }
 
 
-        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        private static void Bot_OnMessage(object sender, MessageEventArgs e)
         {
             var bot = sender as TelegramBotClient;
-            string userMess = e.Message.Text;
-            var userMessWord = userMess.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var userMess = e.Message.Text;
+            var userMessWord = userMess.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
             var count = userMessWord.Length;
 
 
-            if ((count == 1 && userMessWord[0][0] != '/') || (userMessWord[0].ToString() == "/share"))
+            if (count == 1 && userMessWord[0][0] != '/' || userMessWord[0] == "/share")
             {
                 Share_Info(sender, e);
                 return;
             }
 
 
-            if (userMessWord[0].ToString() == "/list")
+            if (userMessWord[0] == "/list")
             {
                 if (List_Share(sender, e))
                 {
@@ -92,11 +72,12 @@ namespace lab4
                     bot = sender as TelegramBotClient;
                     bot.SendTextMessageAsync(e.Message.Chat.Id, "Nothing to show");
                 }
+
                 return;
             }
 
 
-            if (userMessWord[0].ToString() == "/add")
+            if (userMessWord[0] == "/add")
             {
                 if (Add_Several_Shares(userMessWord))
                 {
@@ -109,17 +90,17 @@ namespace lab4
                 else
                 {
                     bot = sender as TelegramBotClient;
-                    if(count > 2)
+                    if (count > 2)
                         bot.SendTextMessageAsync(e.Message.Chat.Id, "Some shares weren't added in list");
                     else
                         bot.SendTextMessageAsync(e.Message.Chat.Id, "Share was not added in list");
                 }
+
                 return;
             }
 
 
-
-            if (userMessWord[0].ToString() == "/help")
+            if (userMessWord[0] == "/help")
             {
                 bot = sender as TelegramBotClient;
                 bot.SendTextMessageAsync(e.Message.Chat.Id, HelpDesk);
@@ -127,9 +108,9 @@ namespace lab4
             }
 
 
-            if (userMessWord[0].ToString() == "/delete")
+            if (userMessWord[0] == "/delete")
             {
-                if (userMessWord[1].ToString() == "&all")
+                if (userMessWord[1] == "&all")
                 {
                     AddedShares.RemoveRange(0, SharesQuant + 1);
                     SharesQuant = -1;
@@ -144,20 +125,21 @@ namespace lab4
                     else
                         bot.SendTextMessageAsync(e.Message.Chat.Id, "Some shares were not deleted from list");
                 }
+
                 return;
             }
 
             bot.SendTextMessageAsync(e.Message.Chat.Id, "Wrong Command");
-
         }
 
-        private static void Share_Info(object sender, Telegram.Bot.Args.MessageEventArgs e, bool shortmess = false, string ShareCode = null, int cost = 0)
+        private static void Share_Info(object sender, MessageEventArgs e, bool shortmess = false,
+            string ShareCode = null, int cost = 0)
         {
             var bot = sender as TelegramBotClient;
 
             var url = "https://finance.yahoo.com/quote/";
 
-            var userMessWord = e.Message.Text.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var userMessWord = e.Message.Text.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
             var count = userMessWord.Length;
 
             if (ShareCode == null)
@@ -165,28 +147,27 @@ namespace lab4
                     ShareCode = userMessWord[1];
                 else
                     ShareCode = userMessWord[0];
-            
+
             url = url + ShareCode + '/';
 
 
-            var handler = new HttpClientHandler { AllowAutoRedirect = true };
+            var handler = new HttpClientHandler {AllowAutoRedirect = true};
             var httpClient = new HttpClient(handler);
 
             var response = httpClient.GetAsync(url).Result;
 
             var redirectUri = response.RequestMessage.RequestUri;
 
-            var web = new HtmlWeb()
-            { };
+            var web = new HtmlWeb();
 
             var doc = web.Load(redirectUri);
 
 
             var elements = doc.DocumentNode.Descendants("div")
-                              .Where(x => x.Attributes["class"] != null)
-                              .Where(x => x.Attributes["class"].Value == "D(ib) Mend(20px)" ||
-                                          x.Attributes["class"].Value == "D(ib) " ||
-                                          x.Attributes["class"].Value == "C($tertiaryColor) Fz(12px)").ToList();
+                .Where(x => x.Attributes["class"] != null)
+                .Where(x => x.Attributes["class"].Value == "D(ib) Mend(20px)" ||
+                            x.Attributes["class"].Value == "D(ib) " ||
+                            x.Attributes["class"].Value == "C($tertiaryColor) Fz(12px)").ToList();
 
             if (elements.Count == 0)
             {
@@ -196,11 +177,10 @@ namespace lab4
             }
 
             var descHtml = elements.SelectMany(x => x.Descendants("span"))
-                                   .Where(x => x.Attributes["class"] != null)
-                                   .FirstOrDefault(x => x.Attributes["class"].Value == "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)");
+                .Where(x => x.Attributes["class"] != null)
+                .FirstOrDefault(x => x.Attributes["class"].Value == "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)");
 
             var SharesCost = descHtml.InnerText;
-
 
 
             descHtml = elements.SelectMany(x => x.Descendants("h1"))
@@ -210,16 +190,14 @@ namespace lab4
             var ShareName = descHtml.InnerText;
 
 
-
             descHtml = elements.SelectMany(x => x.DescendantsAndSelf("div"))
                 .Where(x => x.Attributes["class"] != null)
                 .FirstOrDefault(x => x.Attributes["class"].Value == "C($tertiaryColor) Fz(12px)");
 
 
             var SharePlat = descHtml.InnerText;
-            var ShareVal = SharePlat[SharePlat.Length - 3].ToString() +
-                           SharePlat[SharePlat.Length - 2].ToString() + SharePlat[SharePlat.Length - 1].ToString();
-
+            var ShareVal = SharePlat[SharePlat.Length - 3] +
+                           SharePlat[SharePlat.Length - 2].ToString() + SharePlat[SharePlat.Length - 1];
 
 
             if (shortmess)
@@ -246,43 +224,42 @@ namespace lab4
 
         private static bool Add_Several_Shares(string[] ShareCodes)
         {
-            for (int i = 1; i < ShareCodes.Length; i += 1)
-            {
-                if(!Add_Share(ShareCodes[i])) return false;
-            }
+            for (var i = 1; i < ShareCodes.Length; i += 1)
+                if (!Add_Share(ShareCodes[i]))
+                    return false;
             return true;
         }
 
         private static bool Del_Share(string ShareCode)
         {
             foreach (var share in AddedShares.ToList())
-            {
-                if (share.Name == ShareCode) AddedShares.Remove(share);
-            }
+                if (share.Name == ShareCode)
+                    AddedShares.Remove(share);
             SharesQuant -= 1;
             return true;
         }
 
         private static bool Del_Several_Shares(string[] ShareCodes)
         {
-            for (int i = 1; i < ShareCodes.Length; i += 1)
-            {
-                if (!Del_Share(ShareCodes[i])) return false;
-            }
+            for (var i = 1; i < ShareCodes.Length; i += 1)
+                if (!Del_Share(ShareCodes[i]))
+                    return false;
             AddedShares = AddedShares.ToList();
             return true;
         }
 
-        private static bool List_Share(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        private static bool List_Share(object sender, MessageEventArgs e)
         {
-            foreach (var share in AddedShares)
-            {
-                Share_Info(sender, e, true, share.Name);
-            }
+            foreach (var share in AddedShares) Share_Info(sender, e, true, share.Name);
 
-            return (SharesQuant != -1);
-
+            return SharesQuant != -1;
         }
 
+        private class Share
+        {
+            public string Name { get; set; }
+
+            public int Cost { get; set; }
+        }
     }
 }
